c1a0155ccce0c4947aa8e52fc29c9623ad735cc8
Steamy <toretordenguden@gmail.com>
2025-12-15 14:30:12 +0100

    Zakarias pulley system installed

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody m_rigidBody;
     [SerializeField] private float moveSpeed;
     [SerializeField] ChainHandler chainHandler;
+    [SerializeField] Transform otherPlayer;
 
     [SerializeField] private KeyCode[] keybinds;
 
@@ -18,6 +20,43 @@ public class PlayerController : MonoBehaviour
     Vector3 otherPlayerDistance;
 
     public bool player1;
+
+    
+    Vector3 m_Gravity = new Vector3(0, -4, 0);
+    float m_DistanceBetweenNodes = 0.5f;
+    int m_ConstraintIterationCount = 4;
+
+    public Vector3 Position;
+    public Vector3 OtherNewPosition;
+    public Vector3 PreviousPosition = Vector3.zero;
+    public Vector3 OtherPreviousPosition = Vector3.zero;
+
+    Vector3 NewPosition;
+
+    private void CalculateNewPosition()
+    {
+        var newPreviousPosition = transform.position;
+        var newOtherPreviousPostion = otherPlayer.transform.position;
+
+        NewPosition = (2 * transform.position) - PreviousPosition +
+                                (m_rigidBody.linearVelocity * (float)Math.Pow(Time.fixedDeltaTime, 2));
+        OtherNewPosition = (2 * otherPlayer.transform.position) - OtherPreviousPosition +
+                        (otherPlayer.GetComponent<Rigidbody>().linearVelocity * (float)Math.Pow(Time.fixedDeltaTime, 2));
+        PreviousPosition = newPreviousPosition;
+        OtherPreviousPosition = newOtherPreviousPostion;
+    }
+    private void FixDistanceBetweenPlayers()    //Applying constraints
+    {
+        CalculateNewPosition();
+
+        var d1 = transform.position - otherPlayer.position;
+        var d2 = d1.magnitude;
+        var d3 = (d2 - chainHandler.maxPlayerDistance) / d2;
+
+        Debug.Log("d1: " + d1 + ", d2: " + d2 + ", d3: " + d3);
+        transform.position -= (d1 * (0.5f * d3));
+        otherPlayer.position += (d1 * (0.5f * d3));
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -34,8 +73,6 @@ public class PlayerController : MonoBehaviour
 
         if (player1) otherPlayerDistance = transform.position - chainHandler.player2.position;
         else otherPlayerDistance = transform.position - chainHandler.player1.position;
-        
-
 
         moveVelocity = new Vector3(inputX, 0, inputY);
 
@@ -43,5 +80,11 @@ public class PlayerController : MonoBehaviour
         moveVelocity *= moveSpeed;
 
         m_rigidBody.AddForce(moveVelocity * Time.deltaTime);
+        m_rigidBody.AddForce(moveVelocity * Time.deltaTime);
+
+        if (otherPlayerDistance.magnitude > chainHandler.maxPlayerDistance)
+        {
+            FixDistanceBetweenPlayers();
+        }
     }
 }
